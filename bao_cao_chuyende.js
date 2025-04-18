592bce8dcaff0756e9112689868bdaad2681d74c
khanhsky8386 <nvkhanh2k5@gmail.com>
2025-04-19 00:21:03 +0700

    up last

--- bao_cao_chuyende.js
+++ bao_cao_chuyende.js
@@ -24,30 +24,28 @@ document.querySelectorAll('.previous').forEach(button => {
     button.addEventListener('click', () => {
         const index = button.dataset.index;
         const container = document.querySelector('.DIVV' + index);
-        container.scrollLeft -= 520;
+        container.scrollLeft -= 620;
     })
 })
 document.querySelectorAll('.DIV-container').forEach(container => {
     container.addEventListener('scroll', () => {
-        document.querySelectorAll('.previous').forEach(button => {
-            if (container.scrollLeft === 0) {
-                button.classList.add('tanghinh');
-            } else {
-                button.classList.remove('tanghinh')
-            }
-        })
-        document.querySelectorAll('.RIGHT').forEach(button=>{
-            if (container.scrollLeft >= 1000) {
-                button2.classList.add('tanghinh');
-            } else {
-                button2.classList.remove('tanghinh')
-            }
-        })
-        
-    })
-})
-window.addEventListener('load',()=>{
-    document.querySelectorAll('.previous').forEach(button => {
+        const index = container.dataset.index;
+        const button = document.querySelector('.pre' + index + '-' + index);
+        const button2 = document.querySelector('.next' + index + '-' + index);
+        if (container.scrollLeft === 0) {
             button.classList.add('tanghinh');
+        } else {
+            button.classList.remove('tanghinh')
+        }
+        if (container.scrollLeft >= 1200) {
+            button2.classList.add('tanghinh');
+        } else {
+            button2.classList.remove('tanghinh')
+        }
     })
 })
+// window.addEventListener('load', () => {
+//     document.querySelectorAll('.previous').forEach(button => {
+//         button.classList.add('tanghinh');
+//     })
+// })
