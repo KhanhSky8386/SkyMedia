08f5439a4b71c70132e0332670ca11e4b80600c8
khanhsky8386 <nvkhanh2k5@gmail.com>
2025-04-18 15:13:59 +0700

    dml

--- bao_cao_chuyende.js
+++ bao_cao_chuyende.js
@@ -1,14 +1,48 @@
-document.querySelector('.xemthem').addEventListener('click', (event) =>{
+document.querySelector('.xemthem').addEventListener('click', (event) => {
     event.stopPropagation()
-    if(document.querySelector('.more').classList.contains('hienThi'))
+    if (document.querySelector('.more').classList.contains('hienThi'))
         document.querySelector('.more').classList.remove('hienThi');
-    else{
+    else {
         document.querySelector('.more').classList.add('hienThi');
     }
 });
 
 
-document.body.addEventListener('click',()=>{
-    if(document.querySelector('.more').classList.contains('hienThi'))
+document.body.addEventListener('click', () => {
+    if (document.querySelector('.more').classList.contains('hienThi'))
         document.querySelector('.more').classList.remove('hienThi');
+})
+
+document.querySelectorAll('.RIGHT').forEach(button => {
+    button.addEventListener('click', () => {
+        const index = button.dataset.index;
+        const container = document.querySelector('.DIVV' + index);
+        container.scrollLeft += 520;
+    })
+})
+document.querySelectorAll('.previous').forEach(button => {
+    button.addEventListener('click', () => {
+        const index = button.dataset.index;
+        const container = document.querySelector('.DIVV' + index);
+        container.scrollLeft -= 520;
+    })
+})
+document.querySelectorAll('.DIV-container').forEach(container => {
+    container.addEventListener('scroll', () => {
+        document.querySelectorAll('.previous').forEach(button => {
+            if (container.scrollLeft === 0) {
+                button.classList.add('tanghinh');
+            } else {
+                button.classList.remove('tanghinh')
+            }
+        })
+        document.querySelectorAll('.RIGHT').forEach(button=>{
+            if (container.scrollLeft >= 1000) {
+                button2.classList.add('tanghinh');
+            } else {
+                button2.classList.remove('tanghinh')
+            }
+        })
+        
+    })
 })
